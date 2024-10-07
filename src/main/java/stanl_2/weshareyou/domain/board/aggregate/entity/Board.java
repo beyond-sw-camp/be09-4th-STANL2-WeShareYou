900b931210c2af903056e0f49435c5e351a0f54d
EuiHyeok <dmlgur0517@daum.net>
2024-10-08 00:16:13 +0900

    Revert "feat: 게시글 생성 기능 추가 #(2)"
    This reverts commit 665b6243181cb71089c86bc98c9dee46c1321652.

--- src/main/java/stanl_2/weshareyou/domain/board/aggregate/entity/Board.java
+++ src/main/java/stanl_2/weshareyou/domain/board/aggregate/entity/Board.java
@@ -1,17 +1,11 @@
 package stanl_2.weshareyou.domain.board.aggregate.entity;
 
 import jakarta.persistence.*;
-import lombok.Data;
 
 @Entity
-@Data
-@Table(name="board")
+@Table(name="Board")
 public class Board {
     @Id
-    @Column(name = "BOARD_ID")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Column(name = "BOARD_TITLE",nullable = false)
-    private String title;
 }
--- src/main/java/stanl_2/weshareyou/domain/board/repository/BoardRepository.java
+++
@@ -1,8 +0,0 @@
-package stanl_2.weshareyou.domain.board.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import stanl_2.weshareyou.domain.board.aggregate.entity.Board;
-
-public interface BoardRepository extends JpaRepository<Board, Integer> {
-
-}

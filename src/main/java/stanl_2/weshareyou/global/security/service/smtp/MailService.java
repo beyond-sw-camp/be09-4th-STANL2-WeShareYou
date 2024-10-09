0687a8c69e962c8b4ce767440315dddfa5cad239
Bang1999 <bangdh1593@gmail.com>
2024-10-10 01:57:42 +0900

    fix: 이메일 전송 구현 완료(#19)

--- src/main/java/stanl_2/weshareyou/global/security/service/smtp/MailService.java
+++ src/main/java/stanl_2/weshareyou/global/security/service/smtp/MailService.java
@@ -5,14 +5,6 @@ import jakarta.mail.MessagingException;
 
 public interface MailService {
 
-//    /* 6자리 난수 생성 */
-//    String createCode();
-//
-//    /* Thymeleaf 기반의 html 파일에 값 넣고 연결하는 메소드 */
-//    String setContext(String code);
-//
-//    MimeMessage createEmailForm(String email) throws MessagingException;
-
     /* 만든 메일 전송 */
     void sendEmail(String toEmail) throws MessagingException;
 

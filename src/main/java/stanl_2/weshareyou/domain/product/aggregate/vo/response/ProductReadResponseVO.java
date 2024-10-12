eb2de3a8095492738f0cacf0750d25876ec16788
yuhyejin <hj_eu@naver.com>
2024-10-12 21:17:03 +0900

    fix: 공유물품 시간 포멧 바꾸기 (#60)

--- src/main/java/stanl_2/weshareyou/domain/product/aggregate/vo/response/ProductReadResponseVO.java
+++ src/main/java/stanl_2/weshareyou/domain/product/aggregate/vo/response/ProductReadResponseVO.java
@@ -3,6 +3,8 @@ package stanl_2.weshareyou.domain.product.aggregate.vo.response;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
+import java.sql.Timestamp;
+
 @Setter
 @Getter
 @AllArgsConstructor
@@ -25,19 +27,19 @@ public class ProductReadResponseVO {
     private String category;
 
     @NotNull
-    private String startAt;
+    private Timestamp startAt;
 
     @NotNull
-    private String endAt;
+    private Timestamp endAt;
 
     @NotNull
     private Boolean rental;
 
     @NotNull
-    private String createdAt;
+    private Timestamp createdAt;
 
     @NotNull
-    private String updatedAt;
+    private Timestamp updatedAt;
 
     @NotNull
     private Long adminId;

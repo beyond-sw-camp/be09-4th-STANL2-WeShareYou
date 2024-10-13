5704ad0c0ac3f323222b55cd4e07737fbb1ab864
EuiHyeok <dmlgur0517@daum.net>
2024-10-13 14:12:23 +0900

    feat: AWS S3 적용하여 이미지 업로드 완료 (#67)

--- src/main/java/stanl_2/weshareyou/domain/board/controller/BoardController.java
+++ src/main/java/stanl_2/weshareyou/domain/board/controller/BoardController.java
@@ -1,12 +1,15 @@
 package stanl_2.weshareyou.domain.board.controller;
 
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import stanl_2.weshareyou.domain.board.aggregate.dto.BoardDTO;
-import stanl_2.weshareyou.domain.board.aggregate.entity.TAG;
 import stanl_2.weshareyou.domain.board.aggregate.vo.request.BoardCreateRequestVO;
 import stanl_2.weshareyou.domain.board.aggregate.vo.request.BoardDeleteRequestVO;
 import stanl_2.weshareyou.domain.board.aggregate.vo.request.BoardUpdateRequestVO;
@@ -14,9 +17,12 @@ import stanl_2.weshareyou.domain.board.aggregate.vo.response.BoardCreateResponse
 import stanl_2.weshareyou.domain.board.aggregate.vo.response.BoardDeleteResponseVO;
 import stanl_2.weshareyou.domain.board.aggregate.vo.response.BoardReadDetailResponseVO;
 import stanl_2.weshareyou.domain.board.aggregate.vo.response.BoardUpdateResponseVO;
-import stanl_2.weshareyou.domain.board.repository.BoardRepository;
 import stanl_2.weshareyou.domain.board.service.BoardService;
+import stanl_2.weshareyou.global.common.exception.CommonException;
+import stanl_2.weshareyou.global.common.exception.ErrorCode;
 import stanl_2.weshareyou.global.common.response.ApiResponse;
+
+import java.io.IOException;
 
 @RestController(value = "boardController")
 @RequestMapping("/api/v1/board")
@@ -25,13 +31,34 @@ public class BoardController {
 
     private final BoardService boardService;
     private final ModelMapper modelMapper;
-    private final BoardRepository boardRepository;
+    private final AmazonS3Client amazonS3Client;
 
     @Autowired
-    public BoardController(BoardService boardService, ModelMapper modelMapper, BoardRepository boardRepository) {
+    public BoardController(BoardService boardService, ModelMapper modelMapper, AmazonS3Client amazonS3Client) {
         this.boardService = boardService;
         this.modelMapper = modelMapper;
-        this.boardRepository = boardRepository;
+        this.amazonS3Client = amazonS3Client;
+    }
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    @PostMapping("/img")
+    public ApiResponse<?> uploadImg(@RequestParam("file") MultipartFile file){
+
+        log.info("값 확인: {}", file);
+
+        try {
+            String fileName=file.getOriginalFilename();
+            String fileUrl= "https://" + bucket + "/" +fileName;
+            ObjectMetadata metadata= new ObjectMetadata();
+            metadata.setContentType(file.getContentType());
+            metadata.setContentLength(file.getSize());
+            amazonS3Client.putObject(bucket, fileName, file.getInputStream(), metadata);
+            return ApiResponse.ok(fileUrl);
+        } catch (IOException e) {
+            throw new CommonException(ErrorCode.INTERNAL_SERVER_ERROR);
+        }
     }
 
     /**

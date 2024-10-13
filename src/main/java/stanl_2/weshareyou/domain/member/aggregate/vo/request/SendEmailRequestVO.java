d8a38bcae0bd76c008add2c0557cfc33d0161b67
Bang1999 <bangdh1593@gmail.com>
2024-10-13 13:34:33 +0900

    fix: 이메일 인증 수정(#77)

--- src/main/java/stanl_2/weshareyou/domain/member/aggregate/vo/request/SendEmailRequestVO.java
+++ src/main/java/stanl_2/weshareyou/domain/member/aggregate/vo/request/SendEmailRequestVO.java
@@ -2,7 +2,6 @@ package stanl_2.weshareyou.domain.member.aggregate.vo.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Getter
@@ -10,11 +9,9 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode
-public class CheckCodeRequestVO {
-    @NotNull(message = "인증할 이메일을 입력해주세요.")
+public class SendEmailRequestVO {
+
+    @NotNull(message = "전송할 이메일을 입력해주세요.")
     @Email
     private String email;
-
-    @NotNull(message = "인증번호를 입력해주세요.")
-    private String code;
 }

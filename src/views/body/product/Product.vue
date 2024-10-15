84528041d72728c010fed20545eb042762b50530
yuhyejin <hj_eu@naver.com>
2024-10-16 01:56:38 +0900

    feat: 공유물품 목록 화면 (#3)

--- src/views/body/product/Product.vue
+++ src/views/body/product/Product.vue
@@ -1,12 +1,22 @@
 <template>
-    <div>
-        <h2>공유 물품 페이지</h2>
+    <div class="container">
+        <h2>주방용품</h2>
+        <ProductList />
     </div>
 </template>
 
 <script setup>
+import ProductList from './ProductList.vue';
+
 </script>
 
 <style scoped>
+.container {
+    padding: 20px;
+}
 
+h2 {
+    font-size: 24px;
+    margin-bottom: 20px;
+}
 </style>

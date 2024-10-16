08d83ca71a15b4e29cb3b9de3656f9229792253a
minseokKim6823 <alstjr6823@naver.com>
2024-10-16 15:39:44 +0900

    feat: FAQ 구현 완료(#17)

---
+++ src/components/faq/tags.vue
@@ -0,0 +1,31 @@
+<template>
+    <span class="rounded-button">
+        {{ text }}
+    </span>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  text: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<style scoped>
+    #rounded-button {
+        display: inline-block;
+        padding: 10px 20px;
+        border-radius: 20px;
+        background-color: #e0e0e0;
+        color: #5f6c80;
+        font-size: 2px;
+        text-align: center;
+        margin: 10px;
+        box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+        cursor: pointer;
+}
+</style>
